507eb4c7d99e9818958ae4afef0030e4e8c3392c
zahidtanveer <zahid_tanveer@live.com>
2024-04-20 12:53:39 -0700

    chore: tenant service added

---
+++ Service/TenantService.cs
@@ -0,0 +1,23 @@
+﻿using MultiTenantReactApp.Models;
+
+namespace MultiTenantReactApp.Service
+{
+    public class TenantService
+    {
+        public List<Tenant> GetTenants()
+        {
+            return new List<Tenant>
+        {
+            new Tenant(1, "foo", true, "default"),
+            new Tenant(2, "bar", true, "dark"),
+            // Add more tenants as needed
+        };
+        }
+
+        public Tenant? GetTenantById(int id)
+        {
+            return GetTenants()?.FirstOrDefault(tenant => tenant.Id == id);
+        }
+    }
+
+}
